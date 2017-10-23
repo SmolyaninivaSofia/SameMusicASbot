c92c22070a0456db71bb26ce9b74893450df769e
SobolevaAlexandra <sania.soboleva@mail.ru>
2017-10-23 11:43:11 +0300

    Add echo bot

---
+++ TelegramBot/EchoTelegramBot.py
@@ -0,0 +1,14 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.reply_to(message,"Hi, how are you?")
+
+@bot.message_handler(func=lambda m:True)
+def echo_all(message):
+    bot.reply_to(message,message.text)
+
+bot.polling()
